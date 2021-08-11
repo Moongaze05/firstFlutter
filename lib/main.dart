76da60ba9a1e14beaaac31e0320dee180a18a9c8
Mishanya <makin99@yandex.ru>
2021-08-11 13:15:36 +0500

    auf

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Не ну это флаттер',
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.lime,
+      ),                         // ... to here.
         home: RandomWords(),
     );
   }
@@ -24,12 +28,47 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  void _pushSaved() {
+    Navigator.of(context).push(
+        CupertinoPageRoute<void>(
+          // NEW lines from here...
+          builder: (BuildContext context) {
+            final tiles = _saved.map(
+                  (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+            );
+            final divided = tiles.isNotEmpty
+                ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+                : <Widget>[];
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Выбранные охуевши'),
+                centerTitle: true,
+              ),
+              body: ListView(children: divided),
+              backgroundColor: Colors.amberAccent,
+            );
+          }, // ...to here.
+    )
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Я в ахуе вообще'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
         backgroundColor: Colors.lime,
         shadowColor: Colors.red,
         centerTitle: true,
@@ -41,6 +80,7 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
+        // itemCount: 5,
         itemBuilder: /*1*/ (context, i) {
           if (i.isOdd) return const Divider(
             color: Colors.green,
@@ -51,20 +91,34 @@ class _RandomWordsState extends State<RandomWords> {
             _suggestions.addAll(generateWordPairs(
               safeOnly: false,
               // top:3,
-            ).take(25)); /*4*/
+            ).take(6)); /*4*/
           }
           return _buildRow(_suggestions[index]);
         });
   }
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: TextStyle(fontSize: 18.0,
             color: Colors.black87,
             letterSpacing: 1.2,
-        )
+        ),
       ),
+        trailing: Icon(   // NEW from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {      // NEW lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },               // ... to here.
     );
   }
 }
